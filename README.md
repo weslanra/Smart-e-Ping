b123540b5918ecbb0cde8ff551f8ebbe09740c9d
Weslan <weslanra@dcomp.ufs.br>
2018-11-06 01:27:47 +0000

    starting page layout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Smart-e-Ping
+# e-smart
+## https://e-smart-weslan.c9users.io/
+
 Database translator for JSON or sending to bases ORION - FIWARE
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Weslan Rezende Alves">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="shortcut icon" href="imagem/icon.png" type="image/x-icon"/>
+        
+        <!-- BOOTSTRAP 4.0 -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <!-- FONT material -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- MAIN -->
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
+    <body>
+        <div class="bg-dark d-flex align-items-center justify-content-center" style="height: 100vh; width: 100vw;">
+            <div class="col-6 offset bg-white border rounded h-75">
+                <ul class="progress-navigation">
+                    <li class="active">Connect database</li>
+                    <li>Step 2</li>
+                    <li>Step 3</li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
